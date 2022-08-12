02848ea2e2d0c1432bfa92b34be66bb20a5a4709
soldierrogers <111144269+soldierrogers@users.noreply.github.com>
2022-08-12 20:13:08 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,15 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Wonders" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          ‘Do you believe in Wonders?’, as I heard her speak, I felt coldness running through my whole body. The voice came out in a whisper, but it still echoed loud in my head. After what felt like an eternity, the Darkness spoke again.
+
+‘No?’ the silent voice continued. It almost sounded playful. ‘You will see them with your own eyes, Henry’ the voice suddenly turned even more sinister - ‘Just as Zachary did in the end’.
+
+Read Wonders: [HERE](https://drive.google.com/drive/u/1/folders/1dNK09Q2DzceXpsrr1putItvw9PgEhiT4)
+
+ <code>pages/index.js</code>
         </p>
       </main>
 
